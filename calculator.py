fcff80433eb97153f6a1f3770315ec1c9b56e248
Marksburningthisaccount <marksbrnngthsccnt@gmail.com>
2024-10-15 22:47:08 +0000

    make if statements that control what math problem check what operator the user selects then does the math problem associated

--- calculator.py
+++ calculator.py
@@ -1,4 +1,13 @@
-num1 = int(input("put your first number: "))
+num1 = int(input("put your first number: ")) #variables for the two numbers
 num2 = int(input("put your second number: "))
-op = input("put the operator (+,-,*,/): ")
+op = input("put the operator (+,-,*,/): ") #asks the user what kind of math problem
 print(num1, op, num2)
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
